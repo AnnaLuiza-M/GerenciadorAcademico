cc7331d3c26279751aa0d636729b8695c3c45613
AnnaLuiza-M <lulumoreira150@gmail.com>
2025-10-15 22:24:34 -0300

    Adicionando o Service

---
+++ src/main/java/service/ProfessorService.java
@@ -0,0 +1,57 @@
+package service;
+
+import dto.ProfessorCreateDTO;
+import dto.ProfessorResponseDTO;
+import dto.ProfessorUpdateDTO;
+import mapper.ProfessorMapper;
+import model.entities.Professor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import repository.ProfessorRepository;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ProfessorService {
+
+    private final ProfessorRepository repository;
+    private final ProfessorMapper mapper;
+
+    public ProfessorService(ProfessorRepository repository, ProfessorMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public ProfessorResponseDTO create(ProfessorCreateDTO dto) {
+        Professor professor = mapper.toEntity(dto);
+        repository.save(professor);
+        return mapper.toResponseDTO(professor);
+    }
+
+    public ProfessorResponseDTO findById(Integer id) {
+        Professor professor = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Professor não encontrado"));
+        return mapper.toResponseDTO(professor);
+    }
+
+    public List<ProfessorResponseDTO> findAll() {
+        return mapper.toResponseDTOList(repository.findAll());
+    }
+
+    public ProfessorResponseDTO update(Integer id, ProfessorUpdateDTO dto) {
+        Professor professor = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Professor não encontrado"));
+
+        mapper.updateFromDto(dto, professor);
+        repository.save(professor);
+
+        return mapper.toResponseDTO(professor);
+    }
+
+    public void delete(Integer id) {
+        Professor professor = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Professor não encontrado"));
+        repository.delete(professor);
+    }
+}
